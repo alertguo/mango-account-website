e2800fded7d85cdaf86fa7b1ea23545bbd4013b2
alertguo <1196614612@qq.com>
2020-12-20 10:29:19 +0800

    deploy

---
+++ precache-manifest.c97cff2ab07e58f38db0520f51592e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd36585b6187fe87fdb",
+    "url": "/mango-account-website/css/app.e6231ce9.css"
+  },
+  {
+    "revision": "4760e392eba5504803e2a3d2de29820e",
+    "url": "/mango-account-website/index.html"
+  },
+  {
+    "revision": "ccd36585b6187fe87fdb",
+    "url": "/mango-account-website/js/app.ba9967fa.js"
+  },
+  {
+    "revision": "a1fbda9f27230d85f81f",
+    "url": "/mango-account-website/js/chunk-vendors.117d8e84.js"
+  },
+  {
+    "revision": "28a03ee60c6dc36a370ef5154535c651",
+    "url": "/mango-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mango-account-website/robots.txt"
+  }
+]);
